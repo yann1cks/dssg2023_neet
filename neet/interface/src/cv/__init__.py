d16f642798a263b7b2143b18b67c45e9bc938377
Yann1ck <50637827+yann1cks@users.noreply.github.com>
2023-07-03 17:14:21 +0100

    remove logs and src from last year

--- neet/interface/src/cv/__init__.py
+++
@@ -1,4 +0,0 @@
-from .utils import *
-from .processing import *
-from .cross_validator import *
-from .search_spaces import *
--- neet/interface/src/params/__init__.py
+++
@@ -1,5 +0,0 @@
-from .filepaths import *
-from .data_pipeline_params import *
-from .model_pipeline_params import *
-from .data_pipeline_arguments import *
-from .model_pipeline_arguments import *
